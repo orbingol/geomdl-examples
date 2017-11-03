6f6b2779f9975545c34cb2a1f10b066020d902d5
Onur R. Bingol <orbingol@iastate.edu>
2017-11-03 18:12:34 -0500

    Update curve examples

--- ex_curve01.py
+++ ex_curve01.py
@@ -5,42 +5,27 @@
     Released under MIT License
     Developed by Onur Rauf Bingol (c) 2016-2017
 """
+from geomdl import BSpline
+from geomdl import utilities
 
-from nurbs import Curve as ns
-from nurbs import utilities as utils
-from matplotlib import pyplot as plt
+# Create a B-Spline curve instance
+curve = BSpline.Curve2D()
 
-# Create a NURBS curve instance
-curve = ns.Curve()
+# Set evaluation delta
+curve.delta = 0.01
 
-# Set up the NURBS curve
-curve.read_ctrlpts("data/CP_Curve1.txt")
+# Set up curve
+curve.read_ctrlpts_from_txt("data/CP_Curve1.txt")
 curve.degree = 4
-# Auto-generate the knot vector
-curve.knotvector = utils.knotvector_autogen(curve.degree, len(curve.ctrlpts))
 
-# Calculate curve points
-curve.evaluate_rational()
+# Auto-generate  knot vector
+curve.knotvector = utilities.generate_knot_vector(curve.degree, len(curve.ctrlpts))
 
-# Arrange control points for plotting
-ctrlpts_x = []
-ctrlpts_y = []
-for pt in curve.ctrlpts:
-    ctrlpts_x.append(pt[0])
-    ctrlpts_y.append(pt[1])
+# Calculate curve
+curve.evaluate()
 
-# Arrange curve points for plotting
-curvepts_x = []
-curvepts_y = []
-for pt in curve.curvepts:
-    curvepts_x.append(pt[0])
-    curvepts_y.append(pt[1])
-
-# Plot using Matplotlib
-plt.figure(figsize=(10.67, 8), dpi=96)
-cppolygon, = plt.plot(ctrlpts_x, ctrlpts_y, "k-.")
-curveplt, = plt.plot(curvepts_x, curvepts_y, "r-")
-plt.legend([cppolygon, curveplt], ["Control Points Polygon", "Evaluated Curve"])
-plt.show()
+# Save control points and evaluated curve points
+curve.save_surfpts_to_csv("curve1.csv")
+curve.save_ctrlpts_to_csv("ctrlpts1.csv")
 
 print("End of NURBS-Python Example")
--- ex_curve02.py
+++ ex_curve02.py
@@ -5,51 +5,27 @@
     Released under MIT License
     Developed by Onur Rauf Bingol (c) 2016-2017
 """
+from geomdl import BSpline
+from geomdl import utilities
 
-from nurbs import Curve as ns
-from nurbs import utilities as utils
-from matplotlib import pyplot as plt
+# Create a BSpline (NUBS) curve instance
+curve = BSpline.Curve()
 
-# Create a NURBS curve instance
-curve = ns.Curve()
-
-# Set up the NURBS curve
-curve.read_ctrlpts("data/CP_Curve2.txt")
+# Set up curve
+curve.read_ctrlpts_from_txt("data/CP_Curve2.txt")
 curve.degree = 3
-# Auto-generate the knot vector
-curve.knotvector = utils.knotvector_autogen(curve.degree, len(curve.ctrlpts))
+
+# Auto-generate knot vector
+curve.knotvector = utilities.generate_knot_vector(curve.degree, len(curve.ctrlpts))
 
 # Evaulate curve
 curve.evaluate()
 
-# Arrange curve points for plotting
-curvepts_x = []
-curvepts_y = []
-for pt in curve.curvepts:
-    curvepts_x.append(pt[0])
-    curvepts_y.append(pt[1])
-
-# Calculate curve tangent at u = 0.6
+# Compute curve tangent at u = 0.6
 curvetan = curve.tangent(0.6)
 
-# Extract slope
-slope = curvetan[1][1] / curvetan[1][0]
-# Increment is used to determine the line size
-increment = 5.0
-# We know the first point, ders[0], and we need the second point to draw the tangent line
-new_x = curvetan[0][0] + increment
-# Find y-point corresponding to the x-point "new_x"
-new_y = (slope * (new_x - curvetan[0][0])) + curvetan[0][1]
-
-# Arrange tangent vector for plotting
-tanlinepts_x = [curvetan[0][0], new_x]
-tanlinepts_y = [curvetan[0][1], new_y]
-
-# Plot using Matplotlib
-plt.figure(figsize=(10.67, 8), dpi=96)
-curveplt, = plt.plot(curvepts_x, curvepts_y, "r-")
-tanline, = plt.plot(tanlinepts_x, tanlinepts_y, "b-")
-plt.legend([curveplt, tanline], ["Evaluated Curve", "Tangent Line (u=0.6)"])
-plt.show()
+# Save control points and evaluated curve points
+curve.save_surfpts_to_csv("curve2.csv")
+curve.save_ctrlpts_to_csv("ctrlpts2.csv")
 
 print("End of NURBS-Python Example")

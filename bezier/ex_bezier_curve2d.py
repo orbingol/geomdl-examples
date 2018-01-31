008e40568f410a1f308e8446e21368ec3295a418
Onur R. Bingol <orbingol@gmail.com>
2018-01-30 19:09:50 -0600

    Update 2D Bezier curve example

--- bezier/ex_bezier_curve2d.py
+++ bezier/ex_bezier_curve2d.py
@@ -19,24 +19,54 @@ except ImportError:
 # Fix file path
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
+#
+# 2D CURVE 1
+#
+
 # Create a B-Spline curve instance (Bezier Curve)
-curve = BSpline.Curve2D()
+curve1 = BSpline.Curve2D()
 
 # Set evaluation delta
-curve.delta = 0.01
+curve1.delta = 0.01
 
 # Set up the Bezier curve
-curve.ctrlpts = [[10, 0], [15, 15], [20, 0]]
-curve.degree = 2
+curve1.ctrlpts = [[10, 0], [15, 15], [20, 0]]
+curve1.degree = 2
 
 # Auto-generate knot vector
-curve.knotvector = utilities.generate_knot_vector(curve.degree, len(curve.ctrlpts))
+curve1.knotvector = utilities.generate_knot_vector(curve1.degree, len(curve1.ctrlpts))
 
 # Evaluate curve
-curve.evaluate()
+curve1.evaluate()
 
 # Draw the control point polygon and the evaluated curve
 if render_curve:
-    vis_comp = VisMPL.VisCurve2D()
-    curve.vis = vis_comp
-    curve.render()
+    vis_comp1 = VisMPL.VisCurve2D()
+    curve1.vis = vis_comp1
+    curve1.render()
+
+#
+# 2D CURVE 2
+#
+
+# Create another B-Spline curve instance (Bezier Curve)
+curve2 = BSpline.Curve2D()
+
+# Set evaluation delta
+curve2.delta = 0.01
+
+# Set up the Bezier curve
+curve2.ctrlpts = [[10, 0], [15, 15], [15, 15], [20, 0]]
+curve2.degree = 3
+
+# Auto-generate knot vector
+curve2.knotvector = utilities.generate_knot_vector(curve2.degree, len(curve2.ctrlpts))
+
+# Evaluate curve
+curve2.evaluate()
+
+# Draw the control point polygon and the evaluated curve
+if render_curve:
+    vis_comp2 = VisMPL.VisCurve2D()
+    curve2.vis = vis_comp2
+    curve2.render()

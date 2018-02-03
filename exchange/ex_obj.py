9b442b5f6ea4dec074afbe0dcedc32ff6ba3606e
Onur R. Bingol <orbingol@gmail.com>
2018-02-03 14:16:57 -0600

    Add exchange module examples

---
+++ exchange/ex_obj.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    Examples for the NURBS-Python Package
+    Released under MIT License
+    Developed by Onur Rauf Bingol (c) 2018
+"""
+from geomdl.shapes import surface
+from geomdl import exchange
+
+cylinder = surface.cylinder(radius=5.0, height=22.5)
+cylinder.delta = 0.05
+
+exchange.save_obj(cylinder, "cylindrical_surface.obj")
+
+# Good to have something here to put a breakpoint
+pass

fb0e3caee927995469e47d77a26a7e4b1af564b6
Onur R. Bingol <orbingol@gmail.com>
2018-02-12 02:16:04 -0600

    Add NURBS surface Bezier decomposition example

---
+++ surface/ex_surface03_decompose.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    Examples for the NURBS-Python Package
+    Released under MIT License
+    Developed by Onur Rauf Bingol (c) 2018
+
+    The surface example is kindly contributed by John-Eric Dufour (@jedufour)
+"""
+import os
+from geomdl import NURBS
+
+# Try to load the visualization module
+try:
+    render_surf = True
+    from geomdl.visualization import VisMPL
+except ImportError:
+    render_surf = False
+
+# Fix file path
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+# Create a NURBS surface instance
+surf = NURBS.Surface()
+
+# Set evaluation delta
+surf.delta = 0.025
+
+# Set up surface
+surf.read_ctrlpts_from_txt("ex_surface03.cptw")
+surf.degree_u = 1
+surf.degree_v = 2
+surf.knotvector_u = [0, 0, 1, 1]
+surf.knotvector_v = [0, 0, 0, 0.25, 0.25, 0.5, 0.5, 0.75, 0.75, 1, 1, 1]
+
+# Decompose the surface
+surfaces = surf.decompose()
+
+# Set evaluation delta for all split surfaces
+surfaces.delta = 0.025
+
+# Draw the control point grid and the evaluated surface
+if render_surf:
+    vis_comp = VisMPL.VisSurfWireframe()
+    surfaces.vis = vis_comp
+    surfaces.render()
+
+# Good to have something here to put a breakpoint
+pass

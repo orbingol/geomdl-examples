6653c832e92098814c81cc2844881405b060ebbd
Onur R. Bingol <orbingol@iastate.edu>
2018-03-28 17:53:09 -0500

    Added an example for save_off function

---
+++ exchange/ex_off.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    Examples for the NURBS-Python Package
+    Released under MIT License
+    Developed by Onur Rauf Bingol (c) 2018
+"""
+from geomdl.shapes import surface
+from geomdl import exchange
+
+cylinder = surface.cylinder(radius=5.0, height=22.5)
+cylinder.delta = 0.01
+
+# Export the surface as a .obj file
+exchange.save_off(cylinder, "cylindrical_surface.off")
+
+# Good to have something here to put a breakpoint
+pass

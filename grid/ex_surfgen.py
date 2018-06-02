3edcbde970ceb690bce809fa96ae1336de50f80d
Onur R. Bingol <orbingol@gmail.com>
2018-06-02 18:02:53 -0500

    Add a more detailed surface generator example

---
+++ grid/ex_surfgen.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    Examples for the NURBS-Python Package
+    Released under the MIT License
+    Developed by Onur Rauf Bingol (c) 2018
+
+    This example illustrates the following operations:
+
+    * Generating a control points grid
+    * Generating a B-Spline surface and using the generated grid as its control points input
+    * Splitting the B-Spline surface
+    * Plotting the split surface using Plotly
+"""
+
+
+from geomdl import BSpline
+from geomdl import CPGen
+from geomdl import utilities
+from geomdl.visualization import VisPlotly
+
+# Generate a control points grid
+surfgrid = CPGen.Grid(50, 100)
+
+# Split the width into 5 equal pieces and the height into 10 equal pieces
+surfgrid.generate(5, 10)
+
+# Generate 4 bumps on the grid
+surfgrid.bumps(4)
+
+# Create a BSpline surface instance
+surf = BSpline.Surface()
+
+# Set degrees
+surf.degree_u = 3
+surf.degree_v = 3
+
+# Get the control points from the generated grid
+surf.ctrlpts2d = surfgrid.grid()
+
+# Set knot vectors
+surf.knotvector_u = utilities.generate_knot_vector(surf.degree_u, surf.ctrlpts_size_u)
+surf.knotvector_v = utilities.generate_knot_vector(surf.degree_v, surf.ctrlpts_size_v)
+
+# Split the surface at u = 0.35
+split_surf = surf.split_u(0.35)
+
+# Set sample size of the split surface
+split_surf.sample_size = 25
+
+# Generate the visualization component and its configuration
+vis_config = VisPlotly.VisConfig(ctrlpts=False, legend=False)
+vis_comp = VisPlotly.VisSurface(vis_config)
+
+# Set visualization component of the split surface
+split_surf.vis = vis_comp
+
+# Plot the split surface
+split_surf.render()
+
+# Good to have something here to put a breakpoint
+pass

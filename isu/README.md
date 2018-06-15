e598409a271163ff8492bf14adce36c2eb578204
Onur R. Bingol <orbingol@gmail.com>
2018-06-15 17:34:56 -0500

    Add smesh reading examples

---
+++ isu/README.md
@@ -0,0 +1,1 @@
+This directory contains some scripts which require some data in a special format that is used in Department of Mechanical Engineering at Iowa State University.
---
+++ isu/ex_smesh.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    Examples for the NURBS-Python Package
+    Released under MIT License
+    Developed by Onur Rauf Bingol (c) 2018
+"""
+
+import os
+from geomdl import exchange
+from geomdl.visualization import VisPlotly as vis
+
+
+# Fix file path
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+# Read a directory containing smesh files
+multi_smesh = exchange.import_smesh_dir('data')
+multi_smesh.sample_size = 5
+
+# Draw the control point grid and the evaluated surface
+vis_config = vis.VisConfig(ctrlpts=False, legend=False)
+vis_comp = vis.VisSurface(vis_config)
+multi_smesh.vis = vis_comp
+multi_smesh.render()
+
+# Save MultiSurface as a .obj file
+exchange.export_obj(multi_smesh, "multisurface01.obj")
+
+# Good to have something here to put a breakpoint
+pass

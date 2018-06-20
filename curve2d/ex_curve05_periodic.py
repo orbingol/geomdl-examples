c57825c27c05f70020102f474bfb5168c4ccb609
Onur R. Bingol <orbingol@gmail.com>
2018-06-20 03:31:36 -0500

    Update periodic curve example

--- curve2d/ex_curve05_periodic.py
+++ curve2d/ex_curve05_periodic.py
@@ -9,13 +9,7 @@
 import os
 from geomdl import BSpline
 from geomdl import utilities
-
-# Try to load the visualization module
-try:
-    render_curve = True
-    from geomdl.visualization import VisMPL
-except ImportError:
-    render_curve = False
+from geomdl.visualization import VisMPL
 
 # Fix file path
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
@@ -23,26 +17,26 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 # Create a BSpline (NUBS) curve instance
 curve = BSpline.Curve()
 
-# Set evaluation delta
-curve.delta = 0.001
-
 # Set degree
 curve.degree = 2
 
 # Set control points for a periodic curve
-curve.ctrlpts = [[1, 0], [-1, 0], [-0.5, 0.5], [0.5, -0.5], [1, 0], [-1, 0]]
+curve.ctrlpts = [[1, 0], [-1, 0], [-1.5, 1.5], [1.5, -1.5], [1, 0], [-1, 0]]
 
 # Knot vector
 curve.knotvector = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
-# Evaulate curve
+# Set evaluation delta
+curve.sample_size = 100
+
+# Evaluate curve
 curve.evaluate()
 
 # Draw the control point polygon and the evaluated curve
-if render_curve:
-    vis_comp = VisMPL.VisCurve2D()
-    curve.vis = vis_comp
-    curve.render()
+vis_config = VisMPL.VisConfig(ctrlpts=False, legend=False)
+vis_comp = VisMPL.VisCurve2D(vis_config)
+curve.vis = vis_comp
+curve.render()
 
 # Good to have something here to put a breakpoint
 pass

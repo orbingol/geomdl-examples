03aacb8b2b4d564f3dfaefc4b24ebc8930eb4107
Onur R. Bingol <orbingol@gmail.com>
2018-07-01 07:42:18 -0500

    Add base adjustment example to surfgen01

--- grid/ex_surfgen01.py
+++ grid/ex_surfgen01.py
@@ -24,10 +24,10 @@ from geomdl.visualization import VisPlotly
 surfgrid = CPGen.Grid(50, 100)
 
 # Split the width into 5 equal pieces and the height into 10 equal pieces
-surfgrid.generate(4, 4)
+surfgrid.generate(32, 32)
 
 # Generate 4 bumps on the grid
-surfgrid.bumps(num_bumps=1, all_positive=False, bump_height=45, base_extent=2)
+surfgrid.bumps(num_bumps=4, all_positive=True, bump_height=45, base_extent=4, base_adjust=1)
 
 # Create a BSpline surface instance
 surf = BSpline.Surface()

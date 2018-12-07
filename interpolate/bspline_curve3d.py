6d916147c06db7c7bd5c93a62d53663cdd943722
Onur Rauf Bingol <orbingol@gmail.com>
2018-12-07 00:34:25 -0600

    Initial commit of the 3-dimensional curve interpolation example

---
+++ interpolate/bspline_curve3d.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    Examples for the NURBS-Python Package
+    Released under MIT License
+    Developed by Onur Rauf Bingol (c) 2018
+"""
+
+from geomdl import interpolate
+from geomdl import exchange
+from geomdl.visualization import VisMPL as vis
+
+# Read data points from a .csv file (ref: https://github.com/idealab-isu/de-la-mo)
+points = exchange.import_csv("bspline_curve3d.cpt")
+
+# Do global curve interpolation
+curve = interpolate.interpolate_curve(points, degree=3)
+
+# Plot the interpolated curve
+curve.delta = 0.005
+curve.vis = vis.VisCurve3D(vis.VisConfig(ctrlpts=False))
+curve.render()
+
+# # Visualize data and evaluated points together
+# import numpy as np
+# import matplotlib.pyplot as plt
+# evalpts = np.array(curve.evalpts)
+# datapts = np.array(points)
+# fig = plt.figure()
+# ax = plt.axes(projection='3d')
+# ax.plot(evalpts[:, 0], evalpts[:, 1], evalpts[:, 2])
+# ax.scatter(datapts[:, 0], datapts[:, 1], datapts[:, 2], color="red")
+# plt.show()

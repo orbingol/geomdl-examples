efe1ba2572a030bb0d1b819c9b5e7003ffbc28e1
Onur Rauf Bingol <orbingol@gmail.com>
2018-12-15 02:36:42 -0600

    Add 3-dimensional curve example for global approximation

---
+++ fitting/approximation/global_curve3d.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    Examples for the NURBS-Python Package
+    Released under MIT License
+    Developed by Onur Rauf Bingol (c) 2018
+
+    3-dimensional curve fitting by global approximation using least squares method
+"""
+
+from geomdl import fitting
+from geomdl import exchange
+from geomdl.visualization import VisMPL as vis
+
+# Read data points from a .csv file (ref: https://github.com/idealab-isu/de-la-mo)
+points = exchange.import_csv("ex_curve3d.cpt")
+
+# Do global curve interpolation
+curve = fitting.approximate_curve(points, degree=2, ctrlpts_size=30)
+
+# Plot the interpolated curve
+curve.delta = 0.005
+curve.vis = vis.VisCurve3D(vis.VisConfig(ctrlpts=False))
+# curve.render()
+
+# # Visualize data and evaluated points together
+# import numpy as np
+# import matplotlib.pyplot as plt
+# evalpts = np.array(curve.evalpts)
+# datapts = np.array(points)
+# fig = plt.figure()
+# ax = plt.axes(projection='3d')
+# ax.plot(evalpts[:, 0], evalpts[:, 1], evalpts[:, 2])
+# ax.scatter(datapts[:, 0], datapts[:, 1], datapts[:, 2], color="red")
+# plt.show()

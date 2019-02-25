46bb6b8d020493f2c9d8637900f09e5bf1200351
Onur Rauf Bingol <orbingol@gmail.com>
2019-02-24 23:31:58 -0600

    Simplify curve knot refinement example

--- curve2d/ex_curve01_refinement.py
+++ curve2d/ex_curve01_refinement.py
@@ -9,9 +9,9 @@
 
 import os
 from geomdl import BSpline
-from geomdl import utilities
 from geomdl import exchange
 from geomdl import operations
+from geomdl import knotvector
 from geomdl.visualization import VisMPL
 
 
@@ -21,22 +21,25 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 # Create a B-Spline curve instance
 curve = BSpline.Curve()
 
-# Set up the curve
+# Set degree
 curve.degree = 4
+
+# Set control points
 curve.ctrlpts = exchange.import_txt("ex_curve01.cpt")
 
 # Auto-generate knot vector
-curve.knotvector = utilities.generate_knot_vector(curve.degree, len(curve.ctrlpts))
+curve.knotvector = knotvector.generate(curve.degree, curve.ctrlpts_size)
 
-# Set evaluation delta
-curve.delta = 0.01
-
-# Evaluate curve
-curve.evaluate()
+# Set visualization component
+curve.vis = VisMPL.VisCurve2D()
 
 # Plot the control point polygon and the evaluated curve
-vis_comp = VisMPL.VisCurve2D()
-curve.vis = vis_comp
+curve.render()
+
+# Apply knot refinement w/ density = 0 (no refinement)
+operations.refine_knotvector(curve, [0])
+
+# Plot the control point polygon and the evaluated curve (should be same as the previous one)
 curve.render()
 
 # Apply knot refinement w/ density = 1 (default)

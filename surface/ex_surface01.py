ac574f7ff67de05807d9a61d718847dc4d22a5b5
Onur Rauf Bingol <orbingol@gmail.com>
2020-01-30 12:09:41 -0600

    Update surface01 example

--- surface/ex_surface01.py
+++ surface/ex_surface01.py
@@ -8,9 +8,10 @@
 """
 
 import os
-from geomdl import BSpline
+from geomdl import BSpline, NURBS
 from geomdl import exchange
-from geomdl.visualization import VisVTK as vis
+from geomdl.visualization import VisMPL as vis
+from geomdl import render
 
 
 # Fix file path
@@ -20,15 +21,15 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 surf = BSpline.Surface()
 
 # Set degrees
-surf.degree_u = 3
-surf.degree_v = 3
+surf.degree.u = 3
+surf.degree.v = 3
 
 # Set control points
-surf.set_ctrlpts(*exchange.import_txt("ex_surface01.cpt", two_dimensional=True))
+surf.set_ctrlpts(exchange.import_txt("ex_surface01.cpt"), 6, 6)
 
 # Set knot vectors
-surf.knotvector_u = [0.0, 0.0, 0.0, 0.0, 1.0, 2.0, 3.0, 3.0, 3.0, 3.0]
-surf.knotvector_v = [0.0, 0.0, 0.0, 0.0, 1.0, 2.0, 3.0, 3.0, 3.0, 3.0]
+surf.knotvector.u = [0.0, 0.0, 0.0, 0.0, 1.0, 2.0, 3.0, 3.0, 3.0, 3.0]
+surf.knotvector.v = [0.0, 0.0, 0.0, 0.0, 1.0, 2.0, 3.0, 3.0, 3.0, 3.0]
 
 # Set evaluation delta
 surf.delta = 0.025
@@ -36,13 +37,10 @@ surf.delta = 0.025
 # Evaluate surface points
 surf.evaluate()
 
-# Import and use Matplotlib's colormaps
-from matplotlib import cm
+nsurf = NURBS.Surface.from_bspline(surf)
 
 # Plot the control point grid and the evaluated surface
-vis_comp = vis.VisSurface()
-surf.vis = vis_comp
-surf.render(colormap=cm.cool)
+render.render(nsurf, vis.VisSurfScatter())
 
 # Good to have something here to put a breakpoint
 pass
